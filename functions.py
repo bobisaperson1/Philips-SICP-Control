8d9f979b4e82653db319a20c1c2b5ccb528f1d05
bobisaperson1 <bfulham@bradyfulham.com>
2025-04-17 16:44:43 +0930

    working (as far as i can tell)

--- functions.py
+++ functions.py
@@ -1,61 +1,55 @@
 import socket
 import json
 
-biblefile = "data.JSON"
-
-bible = json.loads(open(biblefile).read())
 
 def Generate_Checksum(data: str):
-    a = data[:-2]
+    a = data
     b = [a[i:i+2] for i in range(0, len(a), 2)] # ['10', 'F8', '00', ...
     sum = "0"
     for i in b:
-        print(i)
         sum = hex(int(sum, 16) ^ int(i, 16))
     return str(sum).replace("0x", "").upper().zfill(2)
 
-def Generate_Command(Control_ID: str, Group: str, Command: str, Data: int=""):
+def Generate_Command(Control_ID: str, Group: str, Command: str, bible, Data: int=[]):
     temp_command = ""
     temp_command += str(len(Data) + 5).zfill(2)
     temp_command += Control_ID
-    print(temp_command)
     temp_command += Group
-    print(temp_command)
     temp_command += Command
     for i in Data:
-        temp_command += Data[i]
+        temp_command += str(i).zfill(2)
     Full_command = temp_command + Generate_Checksum(temp_command)
     print("Command:", Full_command)
-    Decode_Hex(Full_command, "command")
+    Decode_Hex(Full_command, bible, "command")
     return Full_command
 
-def Decode_Hex(Hex, Hex_type="response"):
+def Decode_Hex(Hex, bible, Hex_type="response"):
     print("Decoding", Hex_type)
     a = Hex
     b = [a[i:i+2] for i in range(0, len(a), 2)] # ['10', 'F8', '00', ...
     size = int(b[0])
     control_id = int(b[1])
-    print("Control ID:", int(control_id))
+    print("- Control ID:", int(control_id))
     group = int(b[2])
-    print("Group ID:", int(group))
+    print("- Group ID:", int(group))
     data = b[3:-1]
     command = data[0]
     checksum = b[-1]
     if  int(Generate_Checksum(Hex[:-2]), 16) == int(checksum, 16):
-        print("\033[92mChecksum OK\033[0m")
+        print("- \033[92mChecksum OK\033[0m")
     else:
-        print("\033[91mChecksum failed\033[0m")
-    print("Command:", bible[command]['name'])
+        print("- \033[91mChecksum failed\033[0m")
+    print("- Command:", bible[command]['name'])
     if command in bible:
         for byte in bible[command][Hex_type]:
             type = bible[command][Hex_type][byte]['type']
             match type:
                 case "list":
-                    print(f"{bible[command][Hex_type][byte]['Description']}: {bible[command][Hex_type][byte]['Options'][data[int(byte)]]}")
+                    print(f"- {bible[command][Hex_type][byte]['Description']}: {bible[command][Hex_type][byte]['Options'][data[int(byte)]]}")
                 case "number":
-                    print(f"{bible[command][Hex_type][byte]['Description']}: {data[int(byte)]}")
+                    print(f"- {bible[command][Hex_type][byte]['Description']}: {data[int(byte)]}")
                 case "ASCII":
-                    print(f"{bible[command][Hex_type][byte]['Description']}: {bytes.fromhex(data[int(byte)]).decode('ascii')}")
+                    print(f"- {bible[command][Hex_type][byte]['Description']}: {bytes.fromhex(data[int(byte)]).decode('ascii')}")
 
 def check_response(control_ID, group_ID, response):
     a = response
@@ -65,22 +59,22 @@ def check_response(control_ID, group_ID, response):
     group_id_check = str(b[2]).zfill(2) == group_ID
     data = b[3:-1]
     command = data[0]
-    checksum_check = b[-1] == Generate_Checksum(response[-2])
+    checksum_check = int(Generate_Checksum(response[:-2]), 16) == int(b[-1], 16)
     return control_id_check, group_id_check, data, checksum_check
 
-def retrieve_command(command_name, type):
+def retrieve_command(command_name, type, bible):
     for command in bible:
         if bible[command]["name"] == command_name:
             if bible[command]["type"] == type:
-                print(command)
                 return command
 
 class device:
-    def __init__(self, ip, port, control_ID, group_ID):
+    def __init__(self, ip, port, control_ID, group_ID, biblefile):
         self.ip = ip
         self.port = port
         self.control_ID = str(control_ID).zfill(2)
         self.group_ID = str(group_ID).zfill(2)
+        self.bible = json.loads(open(biblefile).read())
 
     def connect(self):
         host = self.ip
@@ -101,61 +95,30 @@ class device:
 
     def get(self, command: str, *args: int):
         print("Sending data")
-        hex = Generate_Command(self.control_ID, self.group_ID, retrieve_command(command, "Get"), *args)
+        hex = Generate_Command(self.control_ID, self.group_ID, retrieve_command(command, "Get", self.bible), self.bible, args)
         self.connection.send(bytes.fromhex(hex))
         print("Waiting for response")
         data_temp = str(self.connection.recv(1024).hex())
         data = data_temp.replace("\\x", "").replace("b'", "").replace("'", "").upper()
         print('Received:', data)
-        print(check_response(self.control_ID, self.group_ID, data))
+        Decode_Hex(data, self.bible)
         return data
 
-    def set(self, command: str, *args: str):
+    def set(self, command: str, *args: int):
         print("Sending data")
-        hex = Generate_Command(self.control_ID, self.group_ID, retrieve_command(command, "Set"), *args)
+        hex = Generate_Command(self.control_ID, self.group_ID, retrieve_command(command, "Set", self.bible), self.bible, args)
         self.connection.send(bytes.fromhex(hex))
         print("Waiting for response")
         data_temp = str(self.connection.recv(1024).hex())
         data = data_temp.replace("\\x", "").replace("b'", "").replace("'", "").upper()
         print('Received:', data)
-        check_response(data)
+        Decode_Hex(data, self.bible)
         return data
 
-#set commands
-#Set_Power_Standby = "06010018011E"
-#Set_Power_On = "06010018021D"
-#Set_Reboot = "060100570050"
-#Set_Volume_Mute_On = "060100470141"
-#Set_Volume_Mute_Off = "060100470040"
-#Set_tune_to_Channel = "070100C200" # + channel number + Xor checksum
-#Set_Channel_Up_Tune = "060100C301C5"
-#Set_Channel_Down_Tune = "060100C300C4"
-#Set_Volume_Up = "07010041010244"
-#Set_Volume_Down = "07010041000245"
-#Set_Input_Source_Tuner = "090100AC220901008E"
-#Set_Input_Source_HDMI1 = "090100AC0D090100A1"
-#Set_Input_Source_HDMI2 = "090100AC06090100AA"
-#Set_Input_Source_HDMI3 = "090100AC0F090100A3"
-#Set_Input_Source_HDMI4 = "090100AC19090100B5"
-#Set_Input_Source_USB = "090100AC0C090100A0"
-#Set_Input_Source_Smart_Info = "090100AC210901008D"
-#Set_Input_Source_Googlecast = "090100AC230901008F"
-#Set_Input_Source_Custom = "090000AC18000100BC"
 
-#get commands
-#Get_Volume_Mute = "0501004642"
-#Get_Current_Channel_Number = "050100C1C5"
-#Get_Current_Input_Source = "050100ADA9"
-
-TV = device("10.0.0.123", 5000, 1, 0)
+TV = device("10.0.0.123", 5000, 1, 0, "data.json")
 TV.connect()
-#TV.connection.send(bytes.fromhex(Generate_Command("01", "00", "AC", {1: "05", 2: "00", 3: "01", 4: "00"})))
-#print("")
-#print("Waiting for response")
-#data_temp = str(TV.connection.recv(1024).hex())
-#data = data_temp.replace("\\x", "").replace("b'", "").replace("'", "").upper()
-#print('Received:', data)
 TV.get("Power State")
+input("enter to continue")
+TV.set("Power State", 1) 
 TV.disconnect()
-#print("")
-#Decode_Hex(data)
